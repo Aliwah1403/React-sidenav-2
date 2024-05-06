60582e83aa463148e3c6dc2cae78c3f9591edb4d
My pc <mypc@TamaraMacBook-Air.local>
2024-05-06 16:25:59 +0300

    Added custom font and set it up on tailwind config

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-    <h1 className='text-3xl font-bold underline'>Tester</h1>
-    </>
+   <main className="w-full h-screen flex flex-row relative">
+    <section className=" flex flex-col p-10 ml-20 w-full gap-5">
+      <h1 className="text-4xl text-neutral-200">Dashboard</h1>
+      <div className="w-full h-80 border border-neutral-500/50 bg-neutral-800/20 rounded"/>
+      <div className="flex flex-row gap-5 w-full">
+        <div className="border-neutral-500/50 h-60 w-1/2 bg-neautral-800/20 rounded border"></div>
+        <div className="border-neutral-500/50 h-60 w-1/2 bg-neautral-800/20 rounded border"></div>
+      </div>
+    </section>
+   </main>
   )
 }
 
